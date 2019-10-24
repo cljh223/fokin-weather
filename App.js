21a761f803afba9659f5c71022331a283279c8ba
CHO JUNSEOK <CHOJUNSEOK@USERnoMacBook-Pro.local>
2019-10-24 21:33:06 +0900

    second

--- App.js
+++ App.js
@@ -1,32 +1,39 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
+import * as Loaction from "expo-location";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.blueiew}>
-        <Text>hello blue</Text>
-      </View>
-      <View style={styles.yellowView}>
-        <Text>hello yellow</Text>
-      </View>
-    </View>
-  );
-}
+const API_KEY = "b3cec0fbdb6b14e138f40b6b70963caf";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  text: {
-    color: "black"
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow"
-  },
-  blueiew: {
-    flex: 1,
-    backgroundColor: "blue"
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
+  getLocation = async () => {
+    try {
+      await Loaction.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Loaction.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      alert.alert("Can't find you.", "So sad");
+    }
+  };
+
+  componentDidMount() {
+    this.getLocation();
   }
-});
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading></Loading> : null;
+  }
+}
